e3018fb5f56f2d5e9faca0260671532de0151fa3
sakshi3326 <saqkshiiitian123@gmail.com>
2023-12-17 00:08:48 +0530

    connect redux with screens

---
+++ App.js
@@ -0,0 +1,15 @@
+// App.js
+import React from 'react';
+import { Provider } from 'react-redux';
+import Navigation from './src/Navigation';
+import store from './src/store';
+
+const App = () => {
+  return (
+    <Provider store={store}>
+      <Navigation />
+    </Provider>
+  );
+};
+
+export default App;
