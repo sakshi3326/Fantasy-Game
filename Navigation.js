6b5b6db78cb459ac9df856bc68041dd444fac539
sakshi3326 <saqkshiiitian123@gmail.com>
2023-12-16 23:21:07 +0530

    started with basic 2 files

---
+++ Navigation.js
@@ -0,0 +1,21 @@
+// Navigation.js
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import HomeScreen from './screens/HomeScreen';
+
+const Stack = createStackNavigator();
+
+const Navigation = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        {/* Add more screens as needed */}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default Navigation;
